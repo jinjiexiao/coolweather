5ed8751e952771c446a014bf9b79f99fda779c4f
Tony <tony@gmail/com>
2017-06-23 23:43:53 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/xiao/coolweather/db/Country.java
@@ -0,0 +1,46 @@
+package com.example.xiao.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by xiao on 2017/6/23.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
